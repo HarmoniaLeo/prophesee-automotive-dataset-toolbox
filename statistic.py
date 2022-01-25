402e478979953eb6a8f84547f292e3df169b79c0
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-01-25 12:40:31 +0800

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -36,6 +36,8 @@ def make_print_to_file(path='./'):
     #############################################################
     print(fileName.center(60,'*'))
 
+time_window = 200000
+
 def inv_statistic(data_path,item):
     bbox_file = os.path.join(data_path, item[:-6]+"bbox.npy")
     f_bbox = open(bbox_file, "rb")
@@ -44,10 +46,39 @@ def inv_statistic(data_path,item):
     f_bbox.close()
     unique_ts, unique_indices = np.unique(dat_bbox['t'], return_index=True)
     unique_inv = np.unique(unique_ts[1:] - unique_ts[:-1])
-    if np.sum(np.where(unique_inv<=200000)):
+    if np.sum(np.where(unique_inv<=time_window)):
         print(item,unique_inv)
 
-result_path = 'result_inv'
+def full_statistic(data_path,item):
+    event_file = os.path.join(data_path, item)
+    bbox_file = os.path.join(data_path, item[:-6]+"bbox.npy")
+    f_bbox = open(bbox_file, "rb")
+    start, v_type, ev_size, size = npy_events_tools.parse_header(f_bbox)
+    dat_bbox = np.fromfile(f_bbox, dtype=v_type, count=-1)
+    f_bbox.close()
+    unique_ts, unique_indices = np.unique(dat_bbox['t'], return_index=True)
+    f_event = PSEELoader(event_file)
+    for bbox_count,unique_time in enumerate(unique_ts):
+        start_time = unique_time - time_window
+        if start_time >= 0:
+            f_event.seek_time(start_time)
+            events = f_event.load_delta_t(time_window)
+            H, W = 304, 240
+            x, y, t, p = events.T
+            x = x.astype(np.int)
+            y = y.astype(np.int)
+
+            img_pos = np.zeros((H * W,), dtype="float32")
+            img_neg = np.zeros((H * W,), dtype="float32")
+
+            np.add.at(img_pos, x[p == 1] + W * y[p == 1], 0.05)
+            np.add.at(img_neg, x[p == -1] + W * y[p == -1], 0.05)
+
+            img_pos = np.where(img_pos>0,1,0)
+            img_neg = np.where(img_neg>0,1,0)
+            print(event_file,unique_time,np.sum(img_pos)/len(img_pos),np.sum(img_neg)/len(img_neg))
+
+result_path = 'result_full'
 make_print_to_file(path=result_path)
 data_folders = ["train","test","val"]
 data_path="/data/ATIS_Automotive_Detection_Dataset/detection_dataset_duration_60s_ratio_1.0"
@@ -58,7 +89,7 @@ for data_folder in data_folders:
         lastStamp=0
         if os.path.isfile(os.path.join(final_path, item)):
             if item.endswith(".dat"):
-                inv_statistic(final_path,item)
+                full_statistic(final_path,item)
                 
                 '''video = PSEELoader(os.path.join(data_path, item[:-8]+"td.dat"))
                 while not video.done:
