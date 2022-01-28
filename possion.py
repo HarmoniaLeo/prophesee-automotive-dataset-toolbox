dcb2df337002d18724a33933eb702c370ecec1bc
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-01-28 17:59:43 +0800

    Update possion.py

--- possion.py
+++ possion.py
@@ -33,6 +33,7 @@ def denseToSparse(dense_tensor):
     locations = np.stack(non_zero_indices, axis=-1)
 
     select_indices = np.split(locations,range(len(locations)))
+    print(select_indices)
     features = dense_tensor[select_indices]
 
     return locations, features
