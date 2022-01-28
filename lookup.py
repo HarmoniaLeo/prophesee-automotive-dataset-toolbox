18d9b3a1e4a2ce5c5b7fad726ea956bd92e205ce
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-01-28 15:47:17 +0800

    update

---
+++ lookup.py
@@ -0,0 +1,115 @@
+import numpy as np
+from src.io.psee_loader import PSEELoader
+from src.io import npy_events_tools
+import os
+import cv2
+import argparse
+
+def generate_event_volume(events,shape,bins=5):
+    H, W = shape
+    x, y, t, p = events.T
+
+    x, y, p = x.astype(np.int), y.astype(np.int), p.astype(np.int)
+
+    try:
+        t_min = np.min(t)
+        t_max = np.max(t)
+        t = t.astype(np.float)
+        t = (t-t_min)/(t_max-t_min+1e-8)
+
+        t_star = (bins-1)*t[:,None]
+        
+        xpos = x[p == 1]
+        ypos = y[p == 1]
+        adderpos = np.arange(bins)[None,:]
+        adderpos = 1 - np.abs(adderpos-t_star[p == 1])
+        adderpos = np.where(adderpos>=0,adderpos,0)
+
+        xneg = x[p == 0]
+        yneg = y[p == 0]
+        adderneg = np.arange(bins)[None,:]
+        adderneg = 1 - np.abs(adderneg-t_star[p == 0])
+        adderneg = np.where(adderneg>=0,adderneg,0)
+
+        img_pos = np.zeros((H * W , bins),dtype=float)
+        np.add.at(img_pos, W * ypos + xpos, adderpos)
+        img_neg = np.zeros((H * W , bins),dtype=float)
+        np.add.at(img_neg, W * yneg + xneg, adderneg)
+    except Exception:
+        img_pos = np.zeros((H * W , bins),dtype=float)
+        img_neg = np.zeros((H * W , bins),dtype=float)
+
+    histogram = np.concatenate([img_neg, img_pos], -1).reshape((H, W, bins*2))
+
+    return histogram
+
+LABELMAP = ["car", "pedestrian"]
+
+def draw_bboxes(img, boxes, dt = 0, labelmap=LABELMAP):
+    """
+    draw bboxes in the image img
+    """
+    colors = cv2.applyColorMap(np.arange(0, 255).astype(np.uint8), cv2.COLORMAP_HSV)
+    colors = [tuple(*item) for item in colors.tolist()]
+
+    for i in range(boxes.shape[0]):
+        pt1 = (int(boxes[i,1]), int(boxes[i,2]))
+        size = (int(boxes[i,3]), int(boxes[i,4]))
+        pt2 = (pt1[0] + size[0], pt1[1] + size[1])
+        score = boxes[i,-2]
+        class_id = boxes[i,-3]
+        class_name = labelmap[int(class_id)]
+        color = colors[(dt+1) * 60]
+        center = ((pt1[0] + pt2[0]) // 2, (pt1[1] + pt2[1]) // 2)
+        cv2.rectangle(img, pt1, pt2, color, 1)
+        cv2.putText(img, class_name, (center[0], pt2[1] - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
+        cv2.putText(img, str(score), (center[0], pt1[1] - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
+
+def visualizeVolume(volume,gt,filename,path):
+    img = 127 * np.ones((volume.shape[1], volume.shape[2], 3), dtype=np.uint8)
+    for i in range(0,volume.shape[0]//2):
+        c_p = volume[i]
+        c_p = 127 * c_p / np.percentile(c_p,0.9)
+        c_p = np.where(c_p>127, 127, c_p)
+        c_n = volume[i+volume.shape[0]//2]
+        c_n = 127 * c_n / np.percentile(c_n,0.9)
+        c_n = np.where(c_n>127, 127, c_n)
+        img_s = img + c_p[:,:,None].astype(np.uint8) - c_n[:,:,None].astype(np.uint8)
+        draw_bboxes(img_s,gt)
+        path_t = os.path.join(path,filename+"_{0}".format(int(gt[0,0])))
+        if not(os.path.exists(path_t)):
+            os.mkdir(path_t)
+        cv2.imwrite(os.path.join(path_t,'{0}.png'.format(i)),img_s)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='visualize one or several event files along with their boxes')
+    parser.add_argument('-item', type=str)
+    parser.add_argument('-start', type=int)
+    parser.add_argument('-end', type=int)
+
+    args = parser.parse_args()
+
+    result_path = 'result_lookup'
+    data_folder = 'test'
+    item = args.item
+    time_stamp_start = args.start
+    time_stamp_end = args.end
+    data_path = "/data/ATIS_Automotive_Detection_Dataset/detection_dataset_duration_60s_ratio_1.0"
+    final_path = os.path.join(data_path,data_folder)
+    event_file = os.path.join(data_path, item+"_td.dat")
+    bbox_file = os.path.join(data_path, item+"_bbox.npy")
+    f_bbox = open(bbox_file, "rb")
+    start, v_type, ev_size, size = npy_events_tools.parse_header(f_bbox)
+    dat_bbox = np.fromfile(f_bbox, dtype=v_type, count=-1)
+    f_bbox.close()
+    unique_ts, unique_indices = np.unique(dat_bbox['t'], return_index=True)
+    target = dat_bbox[(unique_ts>=time_stamp_start)&(unique_ts<=time_stamp_end)]
+    print(target)
+    f_event = PSEELoader(event_file)
+    f_event.seek_time(time_stamp_start)
+    events = f_event.load_delta_t(time_stamp_end - time_stamp_start)
+    x,y,t,p = events['x'], events['y'], events['ts'], events['p']
+    events = np.stack([x.astype(np.int), y.astype(np.int), 0, t, p], axis=-1)
+    volume = generate_event_volume(events,(240,304))
+    visualizeVolume(volume,target,item,result_path)
---
+++ possion.py
@@ -0,0 +1,47 @@
+import numpy as np
+import scipy
+
+def generate_event_histogram(events, shape):
+    """
+    Events: N x 4, where cols are x, y, t, polarity, and polarity is in {0,1}. x and y correspond to image
+    coordinates u and v.
+    """
+    H, W = shape
+    x, y, t, p = events.T
+    x = x.astype(np.int)
+    y = y.astype(np.int)
+
+    img_pos = np.zeros((H * W,), dtype="float32")
+    img_neg = np.zeros((H * W,), dtype="float32")
+
+    np.add.at(img_pos, x[p == 1] + W * y[p == 1], 1)
+    np.add.at(img_neg, x[p == 0] + W * y[p == 0], 1)
+
+    histogram = np.stack([img_neg, img_pos], -1).reshape((H, W, 2))
+
+    return histogram
+
+def denseToSparse(dense_tensor):
+    """
+    Converts a dense tensor to a sparse vector.
+
+    :param dense_tensor: BatchSize x SpatialDimension_1 x SpatialDimension_2 x ... x FeatureDimension
+    :return locations: NumberOfActive x (SumSpatialDimensions + 1). The + 1 includes the batch index
+    :return features: NumberOfActive x FeatureDimension
+    """
+    non_zero_indices = np.nonzero(np.abs(dense_tensor).sum(axis=-1))
+    locations = np.concatenate((non_zero_indices[:, 1:], non_zero_indices[:, 0, None]), axis=-1)
+
+    select_indices = non_zero_indices.split(1, axis=1)
+    features = np.squeeze(dense_tensor[select_indices], axis=-2)
+
+    return locations, features
+
+def possioned_events(events, start_time, end_time, possion_window = 1000):
+    time_window = end_time - start_time
+    n = time_window//possion_window
+    l = generate_event_histogram(events)/n
+    l = np.repeat(l[:,:,:,None],n,-1)
+    possion_result = np.random.poisson(l)
+    locations, ns = denseToSparse(possion_result)
+    print(locations[:5],ns[:5])
