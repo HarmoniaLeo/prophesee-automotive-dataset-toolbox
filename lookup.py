8a5a8419638c2811b6013bd001e09f22c8839e69
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-01-28 19:34:10 +0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -93,9 +93,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.possion:
-        result_path = 'result_lookup'
-    else:
         result_path = 'result_possion'
+    else:
+        result_path = 'result_lookup'
     if not os.path.exists(result_path):
         os.mkdir(result_path)
     data_folder = 'test'
