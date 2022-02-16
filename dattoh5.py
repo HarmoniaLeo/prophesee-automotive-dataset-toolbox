deeaa83f3edf769edbd309c9fda2a56fa934e035
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-02-17 00:13:19 +0800

    Update dattoh5.py

--- dattoh5.py
+++ dattoh5.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
                     f.create_dataset("bboxes/{0}".format(id), data = bboxes, maxshape=(None, ), chunks=True)
                 else:
                     f_event.seek_event(count_upperbound)
+                    print(end_time,end_count,count_upperbound)
                     events_all = f_event.load_n_events(end_count - count_upperbound)
                     f["events/{0}".format(id)].resize((f["events/{0}".format(id)].shape[0] + len(events_all),))
                     f["events/{0}".format(id)][-len(events_all):] = events_all
