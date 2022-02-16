cf525bd63e0b8580ddb787533bbc486a22f59481
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-02-17 00:08:08 +0800

    Update dattoh5.py

--- dattoh5.py
+++ dattoh5.py
@@ -53,11 +53,10 @@ if __name__ == '__main__':
                 
                 if start_time > time_upperbound:
                     if start_time < 0:
-                        start_count = f_event.seek_time(0)
+                        start_count = 0
+                        f_event.seek_event(0)
                     else:
                         start_count = f_event.seek_time(start_time)
-                    if start_count is None:
-                        print(start_time)
                     id += 1
                     events_all = f_event.load_n_events(end_count - start_count)
                     f.create_dataset("events/{0}".format(id), data = events_all, maxshape=(None, ), chunks=True)
