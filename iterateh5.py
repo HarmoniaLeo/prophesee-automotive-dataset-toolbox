73974a1c71f6d9d924e50f66045c157a8bc7be64
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-02-16 22:13:16 +0800

    Update iterateh5.py

--- iterateh5.py
+++ iterateh5.py
@@ -19,22 +19,13 @@ if __name__ == '__main__':
     final_path = os.path.join(data_path,data_folder)
     h5_file = os.path.join(final_path, item+"_h5.h5")
 
-    f = h5py.File(h5_file, 'w')
+    f = h5py.File(h5_file, 'r')
 
     total_time = 0
-    for idx,data in enumerate(f.keys()):
+    for idx in range(f.attrs["total"]):
         tick = time.time()
-        if (unique_time <= 500000):
-            continue
-        end_time = unique_time
-        end_count = f_event.seek_time(end_time)
-        start_count = end_count - 200000
-        if start_count < 0:
-            start_count = 0
-        f_event.seek_event(start_count)
-        start_time = f_event.current_time
-        bins = (end_time - start_time)//50000 + 1
-        start_time = end_time - bins * 50000
-        events_all = f_event.load_n_events(end_count - start_count)
+        f1 = h5py.File(h5_file, 'r')
+        events = f1["events/{0}".format(idx)]
+        bboxes = f1["bboxes/{0}".format(idx)]
         total_time += time.time() - tick
-    print(total_time/len(unique_ts))
+    print(total_time/f.attrs["total"])
