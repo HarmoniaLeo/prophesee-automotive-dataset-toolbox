c1e4240db5abcd0cd9212a1b5159b8e58da466ba
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-02-20 21:49:45 +0800

    Update remove.py

--- remove.py
+++ remove.py
@@ -3,7 +3,7 @@ import tqdm
 
 data_path = "/data/ATIS_h5/detection_dataset_duration_60s_ratio_1.0"
 
-for data_folder in ['train','val','test']:
+for data_folder in ['train','test']:
     final_path = os.path.join(data_path,data_folder)
     files = [item for item in os.listdir(final_path) if ".dat" in item]
     pbar = tqdm.tqdm(total=len(files), unit='File', unit_scale=True)
