e42d8f6805c0e363397b384f639bb5b21e7ecbc9
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-03-03 16:05:52 +0800

    Update lookup_taf.py

--- lookup_taf.py
+++ lookup_taf.py
@@ -72,6 +72,7 @@ def generate_event_volume(events,shape,time_start,time_end,past_volume=None):
         neg_ecd = np.where(forward_neg, -1e6, 0)
         neg_ecd = np.stack([neg_ecd,neg_ecd])
         img_neg, img_pos = img_neg[:,1:], img_pos[:,1:]
+        neg_ecd, pos_ecd = img_neg[:,1:], img_pos[:,1:]
 
     histogram = np.concatenate([img_neg, img_pos], -1).reshape((H, W, 2)).transpose(2,0,1)
     ecd = np.concatenate([neg_ecd, pos_ecd], -1).reshape((H, W, 2)).transpose(2,0,1)
