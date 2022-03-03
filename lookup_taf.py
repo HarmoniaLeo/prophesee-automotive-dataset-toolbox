1c5a309709d15fc532d4844799dca514484cad1e
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-03-03 16:12:54 +0800

    Update lookup_taf.py

--- lookup_taf.py
+++ lookup_taf.py
@@ -99,9 +99,9 @@ def draw_bboxes(img, boxes, dt = 0, labelmap=LABELMAP):
         cv2.putText(img, str(score), (center[0], pt1[1] - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
 
 def visualizeVolume(volume,ecd,gt_i,filename,path,pct,time_stamp_start,time_stamp_end,i):
-    for j in [1,3]:
+    for j in range(2):
         img_s = 127 * np.ones((volume.shape[1], volume.shape[2], 3), dtype=np.uint8)
-        img = (90 * np.exp(ecd[:,:,j])).astype(np.uint8)
+        img = (90 * np.exp(ecd[j])).astype(np.uint8)
         img_s[:,:,0] = img
         img_s = cv2.cvtColor(img_s, cv2.COLOR_HSV2BGR)
         draw_bboxes(img_s,gt_i)
