60515c68164d4dd40edd5836e7aef6afc30dec11
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-03-03 12:58:46 +0800

    update

--- lookup_taf.py
+++ lookup_taf.py
@@ -10,9 +10,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 sns.set_style("darkgrid")
 
-def generate_event_volume(events,shape,bins=5):
+def generate_event_volume(events,shape,time_start,time_end,past_volume=None):
     H, W = shape
     x, y, t, p = events.T
+
+    ind = (t>=time_start)&(t<=time_end)
+    x, y, t, p = x[ind], y[ind], t[ind], p[ind]
 
     x, y, p = x.astype(np.int), y.astype(np.int), p.astype(np.int)
 
@@ -22,31 +25,57 @@ def generate_event_volume(events,shape,bins=5):
         t = t.astype(np.float)
         t = (t-t_min)/(t_max-t_min+1e-8)
 
-        t_star = bins * t[:,None]
+        t_star = t[:,None]
         
         xpos = x[p == 1]
         ypos = y[p == 1]
-        adderpos = np.arange(1,bins+1)[None,:]
+        adderpos = np.arange(0,2)[None,:]
         adderpos = 1 - np.abs(adderpos-t_star[p == 1])
         adderpos = np.where(adderpos>=0,adderpos,0)
 
         xneg = x[p == 0]
         yneg = y[p == 0]
-        adderneg = np.arange(1,bins+1)[None,:]
+        adderneg = np.arange(0,2)[None,:]
         adderneg = 1 - np.abs(adderneg-t_star[p == 0])
         adderneg = np.where(adderneg>=0,adderneg,0)
 
-        img_pos = np.zeros((H * W , bins),dtype=float)
+        img_pos = np.zeros((H * W , 2),dtype=float)
         np.add.at(img_pos, W * ypos + xpos, adderpos)
-        img_neg = np.zeros((H * W , bins),dtype=float)
+        img_neg = np.zeros((H * W , 2),dtype=float)
         np.add.at(img_neg, W * yneg + xneg, adderneg)
     except Exception:
-        img_pos = np.zeros((H * W , bins),dtype=float)
-        img_neg = np.zeros((H * W , bins),dtype=float)
+        img_pos = np.zeros((H * W , 2),dtype=float)
+        img_neg = np.zeros((H * W , 2),dtype=float)
+    
+    forward_pos = (img_pos==0)
+    forward_neg = (img_neg==0)
+    if not (past_volume is None):
+        img_pos_old, img_neg_old, latest_pos, latest_neg, pos_ecd, neg_ecd = past_volume
+        img_pos_old[:,-1] = np.where(latest_pos,img_pos_old[:,-1] + img_pos[:,0],img_pos_old[:,-1])
+        img_neg_old[:,-1] = np.where(latest_neg,img_neg_old[:,-1] + img_neg[:,0],img_neg_old[:,-1])
+        img_pos = np.concatenate([img_pos_old,img_pos[:,1:]],dim=1)[:,1:]
+        pos_ecd = np.concatenate([pos_ecd,np.zeros_like(pos_ecd[:,-1:])],dim=1)[:,1:]
+        img_neg = np.concatenate([img_neg_old,img_neg[:,1:]],dim=1)[:,1:]
+        neg_ecd = np.concatenate([neg_ecd,np.zeros_like(neg_ecd[:,-1:])],dim=1)[:,1:]
+        for i in range(1,img_pos.shape[1])[::-1]:
+            img_pos[:,i] = np.where(forward_pos, img_pos[:,i-1],img_pos[:,i])
+            pos_ecd[:,i] = np.where(forward_pos, pos_ecd[:,i-1]-1,pos_ecd[:,i])
+            img_neg[:,i] = np.where(forward_neg, img_neg[:,i-1],img_neg[:,i])
+            neg_ecd[:,i] = np.where(forward_neg, neg_ecd[:,i-1]-1,neg_ecd[:,i])
+        img_pos[:,0] = np.where(forward_pos, 0, img_pos[:,0])
+        pos_ecd[:,0] = np.where(forward_pos, -1e6, pos_ecd[:,0])
+        img_neg[:,0] = np.where(forward_neg, 0, img_neg[:,0])
+        neg_ecd[:,0] = np.where(forward_neg, -1e6, neg_ecd[:,0])
+    else:
+        pos_ecd = np.where(forward_pos, -1e6, 0)
+        pos_ecd = np.stack([pos_ecd,pos_ecd])
+        neg_ecd = np.where(forward_neg, -1e6, 0)
+        neg_ecd = np.stack([neg_ecd,neg_ecd])
 
-    histogram = np.concatenate([img_neg, img_pos], -1).reshape((H, W, bins*2)).transpose(2,0,1)
+    histogram = np.concatenate([img_neg, img_pos], -1).reshape((H, W, 2)).transpose(2,0,1)
+    ecd = np.concatenate([neg_ecd, pos_ecd], -1).reshape((H, W, 2)).transpose(2,0,1)
 
-    return histogram
+    return histogram, ecd, (img_pos, img_neg, ~forward_pos, ~forward_neg, pos_ecd, neg_ecd)
 
 LABELMAP = ["car", "pedestrian"]
 
@@ -70,49 +99,17 @@ def draw_bboxes(img, boxes, dt = 0, labelmap=LABELMAP):
         cv2.putText(img, class_name, (center[0], pt2[1] - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
         cv2.putText(img, str(score), (center[0], pt1[1] - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
 
-def visualizeVolume(volume,gt,filename,path,pct,time_stamp_start,time_stamp_end,per_time_bbox=False):
-    step = (time_stamp_end - time_stamp_start)/(volume.shape[0]//2)
-    img = 127 * np.ones((volume.shape[1], volume.shape[2], 3), dtype=np.uint8)
-    for i in range(0,volume.shape[0]//2):
-        if per_time_bbox:
-            gt_i = gt[(dat_bbox['t']>=time_stamp_start + i * step)&(dat_bbox['t']<=time_stamp_start + (i + 1) * step)]
-        else:
-            gt_i = gt[(dat_bbox['t']>=time_stamp_start)&(dat_bbox['t']<=time_stamp_end)]
-        c_p = volume[i+volume.shape[0]//2]
-        c_p_ravel = c_p.reshape(c_p.shape[0]*c_p.shape[1])
-        #c_p = 127 * c_p / np.max(c_p)#np.percentile(c_p,pct)
-        c_p = np.where(c_p>0, 127, c_p)
-        c_n = volume[i]
-        c_n_ravel = c_n.reshape(c_n.shape[0]*c_n.shape[1])
-        #c_n = 127 * c_n / np.max(c_n)#np.percentile(c_n,pct)
-        c_n = np.where(c_n>0, 127, c_n)
-        img_s = img + c_p[:,:,None].astype(np.uint8) - c_n[:,:,None].astype(np.uint8)
+def visualizeVolume(volume,ecd,gt_i,filename,path,pct,time_stamp_start,time_stamp_end,i):
+    for j in [1,3]:
+        img_s = 127 * np.ones((volume.shape[1], volume.shape[2], 3), dtype=np.uint8)
+        img = (90 * np.exp(ecd[:,j])).astype(np.uint8)
+        img_s[:,:,0] = img
+        img_s = cv2.cvtColor(img_s, cv2.COLOR_HSV2BGR)
         draw_bboxes(img_s,gt_i)
         path_t = os.path.join(path,filename+"_start{0}_end{1}".format(int(time_stamp_start),int(time_stamp_end)))
         if not(os.path.exists(path_t)):
             os.mkdir(path_t)
-        cv2.imwrite(os.path.join(path_t,'{0}.png'.format(i)),img_s)
-        sns.distplot(pd.DataFrame({"Negative":c_n_ravel[c_n_ravel>0]}).Negative,label="Negative")
-        sns.distplot(pd.DataFrame({"Positive":c_p_ravel[c_p_ravel>0]}).Positive,label="Positive")
-        #sns.distplot(pd.DataFrame({"Either":(c_n_ravel+c_p_ravel)[(c_n_ravel>0)|(c_p_ravel>0)]}).Either,label="Either")
-        #plt.xlim(-0.3,1.0)
-        plt.xlabel("Value")
-        plt.legend()
-        plt.savefig(os.path.join(path_t,'{0}_kde_overzero.png'.format(i)),dpi=100, bbox_inches = 'tight')
-        plt.clf()
-        sns.distplot(pd.DataFrame({"Negative":c_n_ravel[c_n_ravel>=0]}).Negative,label="Negative")
-        sns.distplot(pd.DataFrame({"Positive":c_p_ravel[c_p_ravel>=0]}).Positive,label="Positive")
-        #sns.distplot(pd.DataFrame({"Either":(c_n_ravel+c_p_ravel)[(c_n_ravel>=0)|(c_p_ravel>=0)]}).Either,label="Either")
-        plt.xlim(-0.1,1.0)
-        plt.xlabel("Value")
-        plt.legend()
-        plt.savefig(os.path.join(path_t,'{0}_kde.png'.format(i)),dpi=100, bbox_inches = 'tight')
-        plt.clf()
-    volume = volume.reshape(2,volume.shape[0]//2,volume.shape[1]*volume.shape[2],1).transpose(0,2,1,3)
-    img = 127 * np.ones((volume.shape[1], volume.shape[2], 3), dtype=np.uint8)
-    img = np.where(volume[0]>0, 0, img)
-    img = np.where(volume[1]>0, 255, img)
-    cv2.imwrite(os.path.join(path_t,'time_view.png'.format(i)),img[:200,:200])
+        cv2.imwrite(os.path.join(path_t,'{0}-{1}.png'.format(i,j)),img_s)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -120,7 +117,7 @@ if __name__ == '__main__':
     parser.add_argument('-item', type=str)
     parser.add_argument('-start', type=int)
     parser.add_argument('-end', type=int)
-    parser.add_argument('-bins', type=int, default=5)
+    parser.add_argument('-window', type=int, default=10000)
     parser.add_argument('-upper_thr', type=float, default=90)
     parser.add_argument('-per_time_bbox', type=bool, default=False)
 
@@ -148,5 +145,11 @@ if __name__ == '__main__':
     x,y,t,p = events['x'], events['y'], events['t'], events['p']
     events = np.stack([x.astype(int), y.astype(int), t, p], axis=-1)
     print(len(events))
-    volume = generate_event_volume(events,(240,304),args.bins)
-    visualizeVolume(volume,dat_bbox,item,result_path,args.upper_thr,time_stamp_start,time_stamp_end,args.per_time_bbox)
+    past_volume = None
+    for i,start in enumerate(range(time_stamp_end, time_stamp_start, args.window)):
+        volume, ecd, past_volume = generate_event_volume(events,(240,304),start,start+args.window,past_volume)
+        if args.per_time_bbox:
+            gt_i = dat_bbox[(dat_bbox['t']>=start)&(dat_bbox['t']<=time_stamp_end)]
+        else:
+            gt_i = dat_bbox[(dat_bbox['t']>=time_stamp_start)&(dat_bbox['t']<=time_stamp_end)]
+        visualizeVolume(volume,ecd,gt_i,item,result_path,args.upper_thr,time_stamp_start,time_stamp_end,i)
