f2a791a4e5562d76a9aa31b76fefc989e0304f30
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-03-20 20:24:57 +0000

    Update lookup_taf_dataset.py

--- lookup_taf_dataset.py
+++ lookup_taf_dataset.py
@@ -30,13 +30,12 @@ def generate_event_volume(events,shape,ori_shape):
     volume[...,1] = np.where(volume[...,1] ==0, -1e6, volume[...,1] + 1)
 
     ecd_view = volume[...,1][volume[...,1] > -1e6]
-    #q90 = np.quantile(ecd_view, 0.99)
-    q90 = 0
+    q90 = np.quantile(ecd_view, 0.90)
     print(q90)
-    q10 = np.quantile(ecd_view, 0.90)
+    q10 = np.quantile(ecd_view, 0.10)
     print(q10)
     volume[...,1] = np.where(volume[...,1] > -1e6, (volume[...,1] - q90)/(q90 - q10) * 2, volume[...,1])
-    volume[...,1] = np.where(volume[...,1] > 0, 0, volume[...,1])
+    #volume[...,1] = np.where(volume[...,1] > 0, 0, volume[...,1])
 
     return volume[...,0], volume[...,1]
 
