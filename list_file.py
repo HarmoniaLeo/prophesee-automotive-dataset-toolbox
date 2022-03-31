3cc373a92f2cdbbcc3f7180c56aff7b86283c784
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-03-31 13:18:54 +0100

    Update list_file.py

--- list_file.py
+++ list_file.py
@@ -5,6 +5,6 @@ files = [(file+"\n").encode("utf-8") for file in files]
 f = open("already_exist.txt", mode='wb')
 f.writelines(files)
 f.close()
-f=open(dir, mode='rb')
+f=open("already_exist.txt", mode='rb')
 files = f.readlines()
 print(files[:5])
