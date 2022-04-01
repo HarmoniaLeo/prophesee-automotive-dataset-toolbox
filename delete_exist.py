02df3d0dea715e819d6e9a586d6f9363baf6e712
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-01 10:18:52 +0100

    Create delete_exist.py

---
+++ delete_exist.py
@@ -0,0 +1,9 @@
+import os
+
+f=open("already_exist.txt", mode='rb')
+files=f.readlines()
+root="/data/Large_taf/train"
+for file in files:
+    file = file.decode("utf-8")[:-1]
+    if os.path.exists(root+"/"+file):
+        os.remove(root+"/"+file)
