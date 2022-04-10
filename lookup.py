f25ded1a7d75d0186fe1158fdea2cac89d88d82c
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-10 14:30:01 +0100

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -118,6 +118,7 @@ def visualizeVolume(volume,gt,filename,path,time_stamp_start,time_stamp_end):
             max_density = points / area
         if points / area < min_density:
             min_density = points / area
+    print("mask")
     density_eff = np.sum(np.sum(np.sum(volume[bbox_mask]>0,axis=0),axis=0),axis=0)/(volume.shape[0]*np.sum(np.sum(np.sum(bbox_mask,axis=0),axis=0),axis=0))
     density_uneff = np.sum(np.sum(np.sum(volume[~bbox_mask]>0,axis=0),axis=0),axis=0)/(volume.shape[0]*np.sum(np.sum(np.sum(~bbox_mask,axis=0),axis=0),axis=0))
     print("density",density)
