d78c5534f82a3f729dacdf888114ffdd6df8c7b7
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-23 20:25:01 +0100

    Update lookup_tore.py

--- lookup_tore.py
+++ lookup_tore.py
@@ -97,5 +97,5 @@ if __name__ == '__main__':
     events = np.stack([x.astype(int), y.astype(int), t, p], axis=-1)
     volume = generate_tore(events,(240,304))
     #volume = np.where(np.log1p(time_stamp_end - volume)>np.log1p(5000000), np.log1p(5000000), np.log1p(time_stamp_end - volume))
-    print(len(volume[volume<1000])/len(volume))
+    print(len(volume[volume<10000])/len(volume))
     #visualizeVolume(volume,dat_bbox[(dat_bbox['t']==time_stamp_end)],item,result_path,time_stamp_end)
