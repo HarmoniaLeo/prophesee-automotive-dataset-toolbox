2b72986bb48d89e10ed9ec6e3b07c7f2d685f7bc
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-23 20:00:38 +0100

    Update lookup_tore.py

--- lookup_tore.py
+++ lookup_tore.py
@@ -99,5 +99,7 @@ if __name__ == '__main__':
     events = np.stack([x.astype(int), y.astype(int), t, p], axis=-1)
     volume = generate_tore(events,(240,304))
     #volume = np.where(np.log1p(time_stamp_end - volume)>np.log1p(5000000), np.log1p(5000000), np.log1p(time_stamp_end - volume))
-    print(volume.max(),volume.min(),volume.mean())
+    sns.histplot(volume)
+    plt.xlabel("Time elapse")
+    plt.savefig()
     #visualizeVolume(volume,dat_bbox[(dat_bbox['t']==time_stamp_end)],item,result_path,time_stamp_end)
