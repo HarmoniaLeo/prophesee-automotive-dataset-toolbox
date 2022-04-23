bcb4627d23809c74c223b203bbb8e721ec99ae1e
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-23 03:40:31 +0100

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -92,11 +92,11 @@ def visualizeVolume(volume,gt,filename,path,time_stamp_start,time_stamp_end):
     c_n = c_n.sum(axis=0)
     #c_map = np.where(c_p>c_n,127,0)
     #c_map = np.where(c_p<c_n,-127,0)
-    c_map = np.where(c_n>0,-127,0)
+    c_map = np.where(c_p>0,127,0)
     #c_map = np.where(c_p>0,127,0)
     img_s = img + c_map.astype(np.uint8)[:,:,None]
     draw_bboxes(img_s,gt,0,LABELMAP)
-    path_t = os.path.join(path,filename+"_{0}_window{1}_neg.png".format(int(time_stamp_end),time_stamp_end-time_stamp_start))
+    path_t = os.path.join(path,filename+"_{0}_window{1}_pos.png".format(int(time_stamp_end),time_stamp_end-time_stamp_start))
     cv2.imwrite(path_t,img_s)
     points_in_view = np.sum(np.sum(np.sum(volume>0,axis=0),axis=0),axis=0)
     density = points_in_view/(volume.shape[0]*volume.shape[1]*volume.shape[2])
