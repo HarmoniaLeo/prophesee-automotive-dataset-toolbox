a5c27d32f291ce639376dcabe670b77c9d17e391
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-23 03:55:17 +0100

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -90,8 +90,13 @@ def visualizeVolume(volume,gt,filename,path,time_stamp_start,time_stamp_end):
     c_p = c_p.sum(axis=0)
     c_n = volume[4:5]
     c_n = c_n.sum(axis=0)
-    c_map = np.where(c_p>c_n,127,0)
-    c_map = np.where(c_p<c_n,-127,c_map)
+    c_p = np.where(c_p>c_n,c_p,0)
+    c_p = c_p/10
+    c_p = np.where(c_p>1.0,127.0,c_p*127)
+    c_n = np.where(c_n>c_p,c_n,0)
+    c_n = c_n/10
+    c_n = np.where(c_n>1.0,-127.0,-c_n*127)
+    c_map = c_p+c_n
     #c_map = np.where(c_n>0,-127,0)
     #c_map = np.where(c_p>0,127,0)
     img_s = img + c_map.astype(np.uint8)[:,:,None]
