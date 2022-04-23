995adefb9bcd8555d96a271624a2ed738bccd1f5
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-23 03:09:54 +0100

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -91,7 +91,7 @@ def visualizeVolume(volume,gt,filename,path,time_stamp_start,time_stamp_end):
     c_n = volume[0::2]
     c_n = c_n.sum(axis=0)
     c_map = np.where(c_p>c_n,127,0)
-    c_map = np.where(c_p<c_n,-127,0)
+    c_map = np.where(c_p<c_n,-127,c_map)
     #c_map = np.where(c_p>0,127,0)
     img_s = img + c_map.astype(np.uint8)[:,:,None]
     draw_bboxes(img_s,gt,0,LABELMAP)
