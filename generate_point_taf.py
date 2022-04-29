43f9fb9f363682fd7de713775b127292e6664ad4
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-29 21:54:28 +0100

    Update generate_point_taf.py

--- generate_point_taf.py
+++ generate_point_taf.py
@@ -96,7 +96,7 @@ target_dir = "/data/lbd/ATIS_taf_point"
 
 total_volume_time = []
 total_taf_time = []
-for mode in ["train"]:
+for mode in ["test"]:
     
     file_dir = os.path.join(raw_dir, mode)
     root = file_dir
@@ -107,7 +107,7 @@ for mode in ["train"]:
     except Exception:
         continue
     # Remove duplicates (.npy and .dat)
-    files = files[int(2*len(files)/3):]
+    #files = files[int(2*len(files)/3):]
     files = [time_seq_name[:-7] for time_seq_name in files
                     if time_seq_name[-3:] == 'dat']
 
