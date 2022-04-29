8f265a89229920d3f135fb21df334cb8ad7e1205
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-04-29 19:29:44 +0100

    update

--- lookup.py
+++ lookup.py
@@ -9,6 +9,59 @@ import matplotlib.pyplot as plt
 import pandas as pd
 sns.set_style("darkgrid")
 
+import time
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.utils import _pair, _quadruple
+
+
+class MedianPool2d(nn.Module):
+    """ Median pool (usable as median filter when stride=1) module.
+    
+    Args:
+         kernel_size: size of pooling kernel, int or 2-tuple
+         stride: pool stride, int or 2-tuple
+         padding: pool padding, int or 4-tuple (l, r, t, b) as in pytorch F.pad
+         same: override padding and enforce same padding, boolean
+    """
+    def __init__(self, kernel_size=3, stride=1, padding=0, same=False):
+        super(MedianPool2d, self).__init__()
+        self.k = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _quadruple(padding)  # convert to l, r, t, b
+        self.same = same
+
+    def _padding(self, x):
+        if self.same:
+            ih, iw = x.size()[2:]
+            if ih % self.stride[0] == 0:
+                ph = max(self.k[0] - self.stride[0], 0)
+            else:
+                ph = max(self.k[0] - (ih % self.stride[0]), 0)
+            if iw % self.stride[1] == 0:
+                pw = max(self.k[1] - self.stride[1], 0)
+            else:
+                pw = max(self.k[1] - (iw % self.stride[1]), 0)
+            pl = pw // 2
+            pr = pw - pl
+            pt = ph // 2
+            pb = ph - pt
+            padding = (pl, pr, pt, pb)
+        else:
+            padding = self.padding
+        return padding
+    
+    def forward(self, x):
+        # using existing pytorch functions and tensor ops so that we get autograd, 
+        # would likely be more efficient to implement from scratch at C/Cuda level
+        x = F.pad(x, self._padding(x), mode='reflect')
+        x = x.unfold(2, self.k[0], self.stride[0]).unfold(3, self.k[1], self.stride[1])
+        x = x.contiguous().view(x.size()[:4] + (-1,)).median(dim=-1)[0]
+        return x
+
 def generate_event_volume(events,shape,bins=5):
     H, W = shape
     x, y, t, p = events.T
@@ -167,4 +220,10 @@ if __name__ == '__main__':
     x,y,t,p = events['x'], events['y'], events['t'], events['p']
     events = np.stack([x.astype(int), y.astype(int), t, p], axis=-1)
     volume = generate_event_volume(events,(240,304),5)
+    volume = torch.from_numpy(volume).cuda()
+    median_filter = MedianPool2d(3,1,same=True).cuda()
+    start = time.time()
+    volume = median_filter(volume)
+    torch.cuda.synchronize()
+    print("elapse",time.time()-start)
     visualizeVolume(volume,dat_bbox,item,result_path,time_stamp_start,time_stamp_end)
