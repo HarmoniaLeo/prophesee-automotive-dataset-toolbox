e7e638a899ecd96ab86f9a8e84ba13da7fb02c50
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-05-24 21:02:28 +0100

    Update lookup_taf_large.py

--- lookup_taf_large.py
+++ lookup_taf_large.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     result_path = 'result_taf_large'
     if not os.path.exists(result_path):
         os.mkdir(result_path)
-    data_folder = 'test'
+    data_folder = 'val'
     item = args.item
     time_stamp_end = args.end
 
