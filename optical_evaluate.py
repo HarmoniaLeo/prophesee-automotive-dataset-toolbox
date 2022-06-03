640dc5d2989d21259fb300220927c7c5f2b6fdee
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-03 21:04:37 +0100

    Update optical_evaluate.py

--- optical_evaluate.py
+++ optical_evaluate.py
@@ -53,9 +53,8 @@ if __name__ == '__main__':
         gt = []
         for file_name in np.unique(file_names_gt):
             dts_file = dts[file_names_dt == file_name]
-            print(dts_file[:5])
             densitys_dt_file = densitys_dt[file_names_dt == file_name]
-            #print(dts_file[:5], densitys_dt_file[:5])
+            print(dts_file[:5], densitys_dt_file[:5])
             gts_file = gts[file_names_gt == file_name]
             densitys_gt_file = densitys_gt[file_names_gt == file_name]
             #print(gts_file[:5], densitys_gt_file[:5])
