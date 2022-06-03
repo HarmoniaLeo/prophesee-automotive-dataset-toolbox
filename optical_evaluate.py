84651c9c11d37e9c77485f53247ee1f4c1987432
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-03 21:06:12 +0100

    Update optical_evaluate.py

--- optical_evaluate.py
+++ optical_evaluate.py
@@ -54,12 +54,11 @@ if __name__ == '__main__':
         for file_name in np.unique(file_names_gt):
             dts_file = dts[file_names_dt == file_name]
             densitys_dt_file = densitys_dt[file_names_dt == file_name]
-            print(dts_file[:5], densitys_dt_file[:5])
             gts_file = gts[file_names_gt == file_name]
             densitys_gt_file = densitys_gt[file_names_gt == file_name]
             #print(gts_file[:5], densitys_gt_file[:5])
             for time_stamp in np.unique(gts_file[:,0]):
-                dts_to_eval = dts_file[(dts_file[:,0] >= time_stamp - args.tol) & (dts_file[:,0] <= time_stamp + args.tol) & (densitys_dt_file >= percentiles[i]) & (densitys_dt_file < percentiles[i+1])]
+                dts_to_eval = dts_file[((dts_file[:,0] >= time_stamp - args.tol) | (dts_file[:,0] <= time_stamp + args.tol)) & (densitys_dt_file >= percentiles[i]) & (densitys_dt_file < percentiles[i+1])]
                 gts_to_eval = gts_file[(gts_file[:,0] == time_stamp) & (densitys_gt_file >= percentiles[i]) & (densitys_gt_file < percentiles[i+1])]
                 if len(gts_to_eval > 0):
                     dt.append(dts_to_eval)
