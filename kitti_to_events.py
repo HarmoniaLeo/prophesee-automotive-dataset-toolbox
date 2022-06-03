b9b142c7de1e8c38122d727dd928a58caa76d0bd
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-03 15:40:32 +0100

    Update kitti_to_events.py

--- kitti_to_events.py
+++ kitti_to_events.py
@@ -11,9 +11,9 @@ from numpy import dtype
 for i in range(0,1):
     file = "/home/lbd/v2e/output/" + str(i).zfill(4) + "/" + str(i).zfill(4) + ".h5"
     if i in [0, 3, 6, 7, 17, 18]:
-        target = open("/data2/lbd/kitti/train" + str(i).zfill(4) + "_td.dat", "wb")
+        target = open("/data2/lbd/kitti/train/" + str(i).zfill(4) + "_td.dat", "wb")
     else:
-        target = open("/data2/lbd/kitti/test" + str(i).zfill(4) + "_td.dat", "wb")
+        target = open("/data2/lbd/kitti/test/" + str(i).zfill(4) + "_td.dat", "wb")
     with h5py.File(file,'r') as f:
         events = f["events"]
         events = rfn.unstructured_to_structured(events, dtype = dtype([('t', '<u8'), ('x', '<f4'), ('y', '<f4'), ('p', '<i4')]))
