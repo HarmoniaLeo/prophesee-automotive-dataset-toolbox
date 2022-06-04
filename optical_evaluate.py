36873bf8703c48c8c6711d84c98662cbc5ba6cf1
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-04 01:58:03 +0100

    Update optical_evaluate.py

--- optical_evaluate.py
+++ optical_evaluate.py
@@ -56,8 +56,7 @@ if __name__ == '__main__':
             densitys_dt_file = densitys_dt[file_names_dt == file_name]
             gts_file = gts[file_names_gt == file_name]
             densitys_gt_file = densitys_gt[file_names_gt == file_name]
-            if len(dts_file) == 0:
-                raise Exception("break")
+            print(dts_file[:20],gts_file[:20])
             #print(gts_file[:5], densitys_gt_file[:5])
             # for time_stamp in np.unique(gts_file[:,0]):
             #     #dts_to_eval = dts_file[(dts_file[:,0] >= time_stamp - args.tol) & (dts_file[:,0] <= time_stamp + args.tol) & (densitys_dt_file >= percentiles[i]) & (densitys_dt_file < percentiles[i+1])]
