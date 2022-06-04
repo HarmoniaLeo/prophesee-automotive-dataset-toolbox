e6ae6fb5f61e8312602041d319b2e8032e150227
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-04 23:40:55 +0100

    Update optical_evaluate.py

--- optical_evaluate.py
+++ optical_evaluate.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         filter_boxes = filter_boxes_gen1
         classes = ['Car', "Pedestrian"]
         #percentiles = [0.0, 0.022496978317470943, 0.03584141107151823, 0.06461365563824012, 0.09765842901836184, 0.13363889435484622, 0.17335709874840652, 0.21913896857877602, 0.2795803473626733, 0.34990456407260045, 0.44142171223006244, 0.557804203925751, 0.7039533807928522, 0.8876761367290176, 1.1264612928414415, 1.4447705627987673, 1.8662489229530281, 2.4500051802140845, 3.242211733282628, 4.436053050035189, 1000]
-        percentiles = [0.0, 0.09765842901836184, 0.2795803473626733, 0.7039533807928522, 1.8662489229530281, 1000]
+        percentiles = [0.0, 0.04511805096746158, 0.19679771615254632, 0.5256959230971759, 1.4566068453489602, 1000]
     else:
         result_path = "/home/liubingde/100-fps-event-det/log/" + args.exp_name + "/summarise.npz"
         shape = [720,1280]
@@ -55,8 +55,10 @@ if __name__ == '__main__':
 
         for i_file, file_name in enumerate(file_names_gt):
 
-            dt_bbox = dts[(file_names_dt == file_name)&(densitys_dt >= percentiles[i])&(densitys_dt < percentiles[i+1])]
-            gt_bbox = gts[(file_names_gt == file_name)&(densitys_gt >= percentiles[i])&(densitys_gt < percentiles[i+1])]
+            #dt_bbox = dts[(file_names_dt == file_name)&(densitys_dt >= percentiles[i])&(densitys_dt < percentiles[i+1])]
+            #gt_bbox = gts[(file_names_gt == file_name)&(densitys_gt >= percentiles[i])&(densitys_gt < percentiles[i+1])]
+            dt_bbox = dts[(file_names_dt == file_name)]
+            gt_bbox = gts[(file_names_gt == file_name)]
 
             dt.append(dt_bbox)
             gt.append(gt_bbox)
@@ -74,5 +76,6 @@ if __name__ == '__main__':
         #             result_boxes_list1.append(l2)
         
         evaluate_detection(gt_boxes_list, result_boxes_list, time_tol = args.tol, classes=classes,height=shape[0],width=shape[1])
+        break
     # print([(percentiles[i] + percentiles[i+1])/2 for i in range(0,len(percentiles)-1)])
     # print(results)
