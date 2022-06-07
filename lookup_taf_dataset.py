6a651ac22c11a197ac12896dff2bde4b9f168be7
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-07 16:17:42 +0100

    Update lookup_taf_dataset.py

--- lookup_taf_dataset.py
+++ lookup_taf_dataset.py
@@ -33,7 +33,7 @@ def minmax_transform(volume):
 def quantile_transform(volume):
     volume = volume.copy()
     ecd_view = volume[...,1][volume[...,1] > -1e8]
-    q90 = np.quantile(ecd_view, 0.9)
+    q90 = np.quantile(ecd_view, 0.95)
     q10 = np.quantile(ecd_view, 0.10)
     volume[...,1] = np.where(volume[...,1] > -1e8, volume[...,1] - q90, volume[...,1])
     volume[...,1] = np.where((volume[...,1] > -1e8) & (volume[...,1] < 0), volume[...,1]/(q90 - q10 + 1e-8) * 6, volume[...,1])
