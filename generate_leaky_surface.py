db56f139db240e1d5933a8e672bb4b34339f9a83
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-10 20:26:29 +0100

    update

--- generate_leaky_surface.py
+++ generate_leaky_surface.py
@@ -26,7 +26,7 @@ def generate_leakysurface(events, q, p, lamda):
             delta = float(events[i,2] - t_prev)
             q = np.where(p - lamda * delta < 0, 0, p - lamda * delta)
             p = q
-            p[int(events[i,1])][int(event[i,0])] += 1
+            p[int(events[i,1])][int(events[i,0])] += 1
         t_prev = events[i,2]
     return q, p
 
