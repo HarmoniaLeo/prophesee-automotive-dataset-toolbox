672aaddbce9375715f03a6c79944127e61811765
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-13 02:24:10 +0100

    Update sampling_dataset3.py

--- sampling_dataset3.py
+++ sampling_dataset3.py
@@ -17,6 +17,24 @@ events_window_total = 50000
 raw_dir = "/data/lbd/Large_Automotive_Detection_Dataset_sampling"
 target_dir = "/data/lbd/Large_Automotive_Detection_Dataset_sampling2"
 
+files_skip = [
+    "moorea_2019-06-17_test_02_000_1037500000_1097500000",
+    "moorea_2019-06-14_002_1220500000_1280500000",
+    "moorea_2019-06-21_000_61500000_121500000",
+    "moorea_2019-06-17_test_01_000_732500000_792500000",
+    "moorea_2019-06-21_001_2196500000_2256500000",
+    "moorea_2019-06-21_000_244500000_304500000",
+    "moorea_2019-06-17_test_02_000_244500000_304500000",
+    "moorea_2019-06-21_000_427500000_487500000",
+    "moorea_2019-06-17_test_02_000_1830500000_1890500000",
+    "moorea_2019-06-17_test_02_000_183500000_243500000",
+    "moorea_2019-06-17_test_01_000_2135500000_2195500000",
+    "moorea_2019-06-19_000_2562500000_2622500000",
+    "moorea_2019-06-17_test_02_000_2623500000_2683500000",
+    "moorea_2019-06-17_test_02_000_2013500000_2073500000",
+    "moorea_2019-06-17_test_02_000_1769500000_1829500000"
+]
+
 for mode in ["train","val","test"]:
     
     file_dir = os.path.join(raw_dir, mode)
@@ -34,6 +52,9 @@ for mode in ["train","val","test"]:
     pbar = tqdm.tqdm(total=len(files), unit='File', unit_scale=True)
 
     for i_file, file_name in enumerate(files):
+        if file_name not in files_skip:
+            continue
+        
         event_file = os.path.join(root, file_name + '_td.dat')
         bbox_file = os.path.join(root, file_name + '_bbox.npy')
         new_event_file = os.path.join(target_root, file_name + '_td.dat')
