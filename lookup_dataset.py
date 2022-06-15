6412ef9fe6a82d78b094276f9e009a16ee7055d8
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-15 23:50:48 +0100

    Update lookup_dataset.py

--- lookup_dataset.py
+++ lookup_dataset.py
@@ -148,6 +148,7 @@ if __name__ == '__main__':
     features = np.right_shift(np.bitwise_and(locations, 2139095040), 23).astype(int)
 
     events = np.stack([x, y, c, p, features], axis=1)
+    print(x.max(),y.max(),c.max(),p.max())
 
     if args.type == "normal":
         C = 5
