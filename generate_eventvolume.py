158488779c318101dc150a84315dbe77c2b183a2
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-16 08:22:33 +0100

    Update generate_eventvolume.py

--- generate_eventvolume.py
+++ generate_eventvolume.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    for mode in ["train","val","test"]:
+    for mode in ["test"]:
         file_dir = os.path.join(raw_dir, mode)
         if not os.path.exists(file_dir):
             os.makedirs(file_dir)
@@ -123,8 +123,8 @@ if __name__ == '__main__':
             
 
         for i_file, file_name in enumerate(files):
-            # if not file_name == "17-04-13_15-05-43_3599500000_3659500000":
-            #     continue
+            if not file_name == "moorea_2019-06-14_000_427500000_487500000":
+                continue
             event_file = os.path.join(root, file_name + '_td.dat')
             bbox_file = os.path.join(root, file_name + '_bbox.npy')
             f_bbox = open(bbox_file, "rb")
