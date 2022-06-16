c2a6e6445ee07f1b76c137f8cfd836c2e1d5643c
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-16 16:53:23 +0100

    Update generate_eventvolume.py

--- generate_eventvolume.py
+++ generate_eventvolume.py
@@ -31,6 +31,8 @@ def generate_agile_event_volume_cuda(events, shape, events_window = 50000, volum
 
     img_viewed = img.view((H, W, img.shape[1] * 2)).permute(2, 0, 1).contiguous()
 
+    print(torch.quantile(img_viewed,0.95))
+
     img_viewed = img_viewed / 5 * 255
 
     return img_viewed
