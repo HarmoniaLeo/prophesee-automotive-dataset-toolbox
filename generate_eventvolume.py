c7513e55e715dc8b0741e98011d5732d9cc10480
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-16 08:27:50 +0100

    Update generate_eventvolume.py

--- generate_eventvolume.py
+++ generate_eventvolume.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
             
 
         for i_file, file_name in enumerate(files):
-            if not file_name == "moorea_2019-02-19_004_td_2013500000_2073500000":
+            if not file_name == "moorea_2019-06-26_test_02_000_1708500000_1768500000":
                 continue
             event_file = os.path.join(root, file_name + '_td.dat')
             bbox_file = os.path.join(root, file_name + '_bbox.npy')
