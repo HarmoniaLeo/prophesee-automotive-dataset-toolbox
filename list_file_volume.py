7c390eca2790dff08cd07ba5357fbbc5191a0e37
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-18 08:08:14 +0100

    Create list_file_volume.py

---
+++ list_file_volume.py
@@ -0,0 +1,10 @@
+import os
+
+files = os.listdir("/data/lbd/Large_Automotive_Detection_Dataset_processed/normal/train")
+files = [(file+"\n").encode("utf-8") for file in files]
+f = open("already_exist.txt", mode='wb')
+f.writelines(files)
+f.close()
+f=open("already_exist.txt", mode='rb')
+files = f.readlines()
+print(files[:5])
