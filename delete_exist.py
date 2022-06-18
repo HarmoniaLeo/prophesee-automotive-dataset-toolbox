401a61f0a1188a2b0375aea5579209d5f9c92033
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-18 08:13:44 +0100

    Update delete_exist.py

--- delete_exist.py
+++ delete_exist.py
@@ -3,8 +3,11 @@ import os
 f=open("already_exist.txt", mode='rb')
 files=f.readlines()
 root="/home/Large_Automotive_Detection_Dataset_processed/taf/train/feature"
+count = 0
 for file in files:
     file = file.decode("utf-8")[:-1]
     if not os.path.exists(root+"/"+file):
         # os.remove(root+"/"+file)
         print(file)
+        count += 1
+print(count)
