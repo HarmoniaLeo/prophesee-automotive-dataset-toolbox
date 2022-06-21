a6a1eb990f81dee7de321350985cca128c546713
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-06-21 10:57:22 +0100

    Update optical_evaluate.py

--- optical_evaluate.py
+++ optical_evaluate.py
@@ -39,8 +39,9 @@ if __name__ == '__main__':
         #percentiles = [0.0, 0.13798373765648486, 0.6976278461290158, 2.025066711356914, 5.278082102864997, 1000]
         #percentiles = [0.0, 0.0, 0.0, 0.017224068593383286, 0.04395048958884435, 0.10717341445938622, 0.20183185315356414, 0.31609882595677286, 0.4509914337597868, 0.6113675070465179, 0.811929173028783, 1.0687768126032453, 1.4313008685647621, 1.9108096336945133, 2.5080834649774104, 3.238813250464127, 4.108326745357363, 5.203311921389142, 6.6210589062068905, 8.648134991471418]
         #percentiles = [0.0, 0.0, 0.0, 0.018915752892064257, 0.0489449954503074, 0.11323120410190246, 0.20295585924830123, 0.3099795391225571, 0.4390496015194756, 0.59493510328393, 0.78934969219668, 1.0402726076857434, 1.3904864304128026, 1.862398078728036, 2.463074497303443, 3.211974451696608, 4.130296275763464, 5.292283327513696, 6.754746824399112, 8.873209713168537]
-        percentiles = [0.0, 0.10717341445938622, 1000]
-        #percentiles = [0.0, 0.0, 0.0, 0.017224068593383286, 0.04395048958884435, 0.10717341445938622, 0.20183185315356414, 0.31609882595677286, 0.4509914337597868, 0.6113675070465179, 0.811929173028783, 1.0687768126032453, 1.4313008685647621, 1.9108096336945133, 2.5080834649774104, 3.238813250464127, 4.108326745357363, 5.203311921389142, 6.6210589062068905, 8.648134991471418]
+        percentiles1 = [0.0, 0.13228603624169974, 1000]
+        #percentiles2 = [0.0, 0.0, 0.0, 0.023422725755273533, 0.059826075336854655, 0.13228603624169974, 0.22620292204172526, 0.3317556131765107, 0.4596359541998162, 0.612762818414821, 0.8057039635919373, 1.0514766044419281, 1.4003382948484702, 1.8652975339956133, 2.4638853265597676, 3.1953989897121904, 4.098562010768772, 5.225311412136522, 6.664381675981891, 8.751820331652807]
+        percentiles2 = [0.0, 0.059826075336854655, 0.4596359541998162, 1.4003382948484702, 4.098562010768772, 1000]
         
         
     bbox_file = result_path
@@ -58,7 +59,36 @@ if __name__ == '__main__':
 
     results = []
 
-    for i in range(0,len(percentiles)-1):
+    for i in range(0,len(percentiles1)-1):
+        print(i,percentiles[i],percentiles[i+1])
+        dt = []
+        gt = []
+
+        for i_file, file_name in enumerate(np.unique(file_names_gt)):
+
+            dt_bbox = dts[(file_names_dt == file_name)&(densitys_dt >= percentiles[i])&(densitys_dt < percentiles[i+1])]
+            gt_bbox = gts[(file_names_gt == file_name)&(densitys_gt >= percentiles[i])&(densitys_gt < percentiles[i+1])]
+            #dt_bbox = dts[(file_names_dt == file_name)]
+            #gt_bbox = gts[(file_names_gt == file_name)]
+
+            dt.append(dt_bbox)
+            gt.append(gt_bbox)
+
+        gt_boxes_list = map(filter_boxes, gt)
+        result_boxes_list = map(filter_boxes, dt)
+        gt_boxes_list1 = []
+        result_boxes_list1 = []
+        for l1,l2 in zip(gt_boxes_list,result_boxes_list):
+            if len(l1) > 0:
+                gt_boxes_list1.append(l1)
+                if len(l2) == 0:
+                    result_boxes_list1.append(np.array([[l1[0,0],0,0,0,0,0,0,0]]))
+                else:
+                    result_boxes_list1.append(l2)
+        
+        result = evaluate_detection(gt_boxes_list1, result_boxes_list1, time_tol = args.tol, classes=classes,height=shape[0],width=shape[1])
+        results.append(result[0])
+    for i in range(0,len(percentiles2)-1):
         print(i,percentiles[i],percentiles[i+1])
         dt = []
         gt = []
