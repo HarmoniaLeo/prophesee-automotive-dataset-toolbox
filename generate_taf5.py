5a659bf1c2124f6027044fa7af9ad777cec4ca70
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-07-01 06:30:51 +0100

    Update generate_taf5.py

--- generate_taf5.py
+++ generate_taf5.py
@@ -68,7 +68,7 @@ def taf_cuda(x, y, t, p, shape, volume_bins, past_volume, filter = False):
 def generate_taf_cuda(events, shape, past_volume = None, volume_bins=5, filter = False):
     x, y, t, p, z = events.unbind(-1)
 
-    x, y, p = x.long(), y.long(), p.long()
+    x, y, t, p = x.long(), y.long(), t.float(), p.long()
     
     histogram_ecd, past_volume = taf_cuda(x, y, t, p, shape, volume_bins, past_volume, filter)
 
