d1e69324b9a5df1158c317ea2296e1778129cfd8
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-07-03 05:44:01 +0100

    Update generate_taf6.py

--- generate_taf6.py
+++ generate_taf6.py
@@ -27,8 +27,10 @@ def taf_cuda(x, y, t, p, shape, volume_bins, past_volume, filter = False):
     img.index_add_(0, p + 2 * x + 2 * W * y, torch.ones_like(x).float())
     t_img_f = torch.zeros((H * W * 2)).float().to(x.device)
     t_img_f.index_add_(0, p + 2 * x + 2 * W * y, 1 - t)
+    print(torch.max(1-t),torch.min(1-t))
     t_img_b = torch.zeros((H * W * 2)).float().to(x.device)
     t_img_b.index_add_(0, p + 2 * x + 2 * W * y, t)
+    print(torch.max(t),torch.min(t))
 
     img = img.view(H, W, 2)
     t_img_f = t_img_f.view(H, W, 2)
