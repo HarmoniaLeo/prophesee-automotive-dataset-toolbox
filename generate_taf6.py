91519b120f84c944b2733dba605ab6979ec67ca3
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-07-07 11:49:28 +0100

    Update generate_taf6.py

--- generate_taf6.py
+++ generate_taf6.py
@@ -107,7 +107,7 @@ def leaky_transform(ecd):
     #print("transform",ecd.max(),ecd.min())
     ecd = torch.where(ecd < 0, torch.zeros_like(ecd), ecd)
     #print("limit",ecd.max(),ecd.min())
-    #ecd = ecd * ecd / (torch.sum(ecd, dim = 0, keepdim=True) + 1e-8)
+    ecd = ecd * ecd / (torch.sum(ecd, dim = 0, keepdim=True) + 1e-8)
     #print("encode",ecd.max(),ecd.min())
     ecd = ecd * 255
     #print("scaling",ecd.max(),ecd.min())
