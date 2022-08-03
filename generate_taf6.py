3a65b61666204a9ae8e7f4560899e275f03fafd0
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-03 01:56:42 +0100

    Update generate_taf6.py

--- generate_taf6.py
+++ generate_taf6.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
     target_dir = args.target_dir
     dataset = args.dataset
 
-    min_event_count = 50000000
+    min_event_count = 10000000
     if dataset == "gen4":
         # min_event_count = 800000
         shape = [720,1280]
