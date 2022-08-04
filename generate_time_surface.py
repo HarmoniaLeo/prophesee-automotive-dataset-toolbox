147bd6db8b23a4c0885040e26563db808d66dd3a
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-04 09:52:07 +0100

    Update generate_time_surface.py

--- generate_time_surface.py
+++ generate_time_surface.py
@@ -105,8 +105,8 @@ if __name__ == '__main__':
         for i_file, file_name in enumerate(files):
             # if not file_name == "17-04-13_15-05-43_3599500000_3659500000":
             #     continue
-            # if not file_name == "moorea_2019-06-26_test_02_000_1708500000_1768500000":
-            #     continue
+            if not file_name == "moorea_2019-06-26_test_02_000_1708500000_1768500000":
+                continue
             event_file = os.path.join(root, file_name + '_td.dat')
             bbox_file = os.path.join(label_root, file_name + '_bbox.npy')
             #h5 = h5py.File(volume_save_path, "w")
