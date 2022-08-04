09ef9ee969681f825716d4ab93a002e69ae446e4
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-04 06:22:01 +0100

    Update generate_time_surface.py

--- generate_time_surface.py
+++ generate_time_surface.py
@@ -144,10 +144,10 @@ if __name__ == '__main__':
                 events = dat_event.load_n_events(int(end_count - start_count))
                 del dat_event
                 events = torch.from_numpy(rfn.structured_to_unstructured(events)[:, [1, 2, 0, 3]].astype(float)).cuda()
-                events = events[-10000000:]
 
                 if not memory is None:
                     events = torch.cat([memory, events])
+                events = events[-10000000:]
                 
                 events = events[events[:, 2] > unique_time - events_window]
                 memory = events.clone()
