7b97631bf0b6142c4de34f57a8236b0f7c3c2b81
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-11 06:05:57 +0100

    Update generate_taf6.py

--- generate_taf6.py
+++ generate_taf6.py
@@ -262,8 +262,8 @@ if __name__ == '__main__':
                         volume, memory = generate_taf_cuda(events_, shape, memory, event_volume_bins)
                         volume = torch.nn.functional.interpolate(volume[None,:,:,:], size = target_shape, mode='nearest')[0]
                 volume = volume.view(event_volume_bins, 2, target_shape[0], target_shape[1])
-                print(volume.mean([1,2,3]))
                 volume = leaky_transform(volume)
+                print(volume.mean([1,2,3]))
                 ecd = volume.cpu().numpy().copy()
                 ecd = np.flip(ecd, axis = 0)
                 #print(ecd.shape)
