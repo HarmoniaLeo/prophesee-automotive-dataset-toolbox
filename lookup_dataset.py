cbf073ea91a9d0033e2a20b43f71dedadee1317d
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2022-08-20 02:00:16 +0100

    Update lookup_dataset.py

--- lookup_dataset.py
+++ lookup_dataset.py
@@ -102,10 +102,10 @@ def visualizeVolume(volume,gt,dt,filename,path,time_stamp_end,tol,LABELMAP,suffi
     img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
 
     gt = gt[gt['t']==time_stamp_end]
-    draw_bboxes(img,gt,0,LABELMAP)
+    #draw_bboxes(img,gt,0,LABELMAP)
     if not (dt is None):
         dt = dt[(dt[:,0]>time_stamp_end-tol)&(dt[:,0]<time_stamp_end+tol)]
-        draw_bboxes(img,dt,1,LABELMAP)
+        #draw_bboxes(img,dt,1,LABELMAP)
         path_t = os.path.join(path,filename+"_{0}_".format(int(time_stamp_end)) + suffix + "_eventvolume_result.png")
     else:
         path_t = os.path.join(path,filename+"_{0}_".format(int(time_stamp_end)) + suffix + "_eventvolume.png")
