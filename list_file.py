220d08c442b4f71482046ffcc4ec5508f9ef8b4e
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2023-03-22 21:04:39 +0800

    update

--- list_file.py
+++ list_file.py
@@ -1,10 +1,11 @@
 import os
 
-files = os.listdir("/datassd4t/lbd/Large_Automotive_Detection_Dataset_sampling/train")
-files = [(file[:-9]+"\n").encode("utf-8") for file in files]
-f = open("already_exist.txt", mode='wb')
-f.writelines(files)
-f.close()
-f=open("already_exist.txt", mode='rb')
-files = f.readlines()
-print(files[:5])
+for data_division in ["train", "test", "val"]:
+    files = os.listdir("/data/lbd/Large_Automotive_Detection_Dataset_sampling/" + data_division)
+    files = [(file[:-9]+"\n").encode("utf-8") for file in files]
+    f = open("already_exist_" + data_division +".txt", mode='wb')
+    f.writelines(files)
+    f.close()
+    f=open("already_exist_" + data_division +".txt", mode='rb')
+    files = f.readlines()
+    print(files[:5])
