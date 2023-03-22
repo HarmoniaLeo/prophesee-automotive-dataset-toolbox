3bb25e214ef864c51d7b55c39bad30b9a1309a3e
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2023-03-22 21:26:42 +0800

    Update copy_no_exist.py

--- copy_no_exist.py
+++ copy_no_exist.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-target_root = "/home/lbd/Large_Automotive_Detection_Dataset_processed/taf3"
+target_root = "/data/lbd/Large_Automotive_Detection_Dataset_processed/taf3"
 if not os.path.exists(target_root):
     os.makedirs(target_root)
 for data_division in ["train", "test", "val"]:
@@ -12,7 +12,7 @@ for data_division in ["train", "test", "val"]:
             os.makedirs(target_root + "/" + data_division + "/" + bin_division)
         f=open("already_exist_" + data_division + "_" + bin_division + ".txt", mode='rb')
         existing_files=f.readlines()
-        root="/home/lbd/Large_Automotive_Detection_Dataset_processed/taf2/" + data_division + "/" + bin_division
+        root="/data/lbd/Large_Automotive_Detection_Dataset_processed/taf2/" + data_division + "/" + bin_division
         count = 0
         for target_file in os.listdir(root):
             if target_file not in existing_files:
