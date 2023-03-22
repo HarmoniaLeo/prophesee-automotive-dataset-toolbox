c19ba11b80ab411a5240a13addc472d13823a430
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2023-03-22 21:23:02 +0800

    Update copy_no_exist.py

--- copy_no_exist.py
+++ copy_no_exist.py
@@ -1,16 +1,21 @@
 import os
+import shutil
 
-if not os.path.exists("/home/lbd/Large_Automotive_Detection_Dataset_processed/taf3"):
-    os.makedirs("/home/lbd/Large_Automotive_Detection_Dataset_processed/taf3")
+target_root = "/home/lbd/Large_Automotive_Detection_Dataset_processed/taf3"
+if not os.path.exists(target_root):
+    os.makedirs(target_root)
 for data_division in ["train", "test", "val"]:
-    f=open("already_exist_" + data_division + ".txt", mode='rb')
-    files=f.readlines()
-    root="/home/lbd/Large_Automotive_Detection_Dataset_processed/taf2/train"
-    count = 0
-    for file in files:
-        file = file.decode("utf-8")[:-1]
-        if not os.path.exists(root+"/"+file):
-            # os.remove(root+"/"+file)
-            print(file)
-            count += 1
-    print(count)
+    if not os.path.exists(target_root + "/" + data_division):
+        os.makedirs(target_root + "/" + data_division)
+    for bin_division in ["bins4", "bins8"]:
+        if not os.path.exists(target_root + "/" + data_division + "/" + bin_division):
+            os.makedirs(target_root + "/" + data_division + "/" + bin_division)
+        f=open("already_exist_" + data_division + "_" + bin_division + ".txt", mode='rb')
+        existing_files=f.readlines()
+        root="/home/lbd/Large_Automotive_Detection_Dataset_processed/taf2/" + data_division + "/" + bin_division
+        count = 0
+        for target_file in os.listdir(root):
+            if target_file not in existing_files:
+                # shutil.copy(root + "/" + target_file, target_root + "/" + data_division + "/" + bin_division)
+                count += 1
+        print(count)
