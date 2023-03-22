18fdd420cc090679e05096fcc92ea4ce3a0f194a
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2023-03-22 21:08:03 +0800

    update

--- list_file_volume.py
+++ list_file_volume.py
@@ -1,10 +1,11 @@
 import os
 
-files = os.listdir("/data/lbd/Large_Automotive_Detection_Dataset_processed/taf2/train/bins4")
-files = [(file+"\n").encode("utf-8") for file in files]
-f = open("already_exist.txt", mode='wb')
-f.writelines(files)
-f.close()
-f=open("already_exist.txt", mode='rb')
-files = f.readlines()
-print(files[:5])
+for data_division in ["train", "test", "val"]:
+    files = os.listdir("/data/lbd/Large_Automotive_Detection_Dataset_processed/taf2/" + data_division + "/bins4")
+    files = [(file+"\n").encode("utf-8") for file in files]
+    f = open("already_exist_" + data_division + ".txt", mode='wb')
+    f.writelines(files)
+    f.close()
+    f=open("already_exist_" + data_division + ".txt", mode='rb')
+    files = f.readlines()
+    print(files[:5])
